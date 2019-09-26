403e7e9ececad34c7dcdfa9b1fc79e33128fe4eb
M. K. V <mvaloy@nmbu.no>
2019-09-26 18:24:51 +0200

    Revrote the variable name in the precoded part of the bubble_sort code. SO as to avid shadow names and follow pep-8. Hope this was OK.

--- src/marie_kolvik_ex/ex02/bubble_sort.py
+++ src/marie_kolvik_ex/ex02/bubble_sort.py
@@ -27,9 +27,9 @@ def bubble_sort(data):
 
 if __name__ == "__main__":
 
-    for data in ((),
+    for info in ((),
                  (1,),
                  (1, 3, 8, 12),
                  (12, 8, 3, 1),
                  (8, 3, 12, 1)):
-        print('{!s:>15} --> {!s:>15}'.format(data, bubble_sort(data)))
+        print('{!s:>15} --> {!s:>15}'.format(info, bubble_sort(info)))
