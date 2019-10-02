368ce54cde80155de92edd85ce8b5ae95c8f0648
M. K. V <mvaloy@nmbu.no>
2019-10-02 12:06:27 +0200

    pasted bubble-sort into the file, would prefer to import it, feels like bad python-programing

---
+++ src/marie_kolvik_ex/ex03_project/test_sorting.py
@@ -0,0 +1,36 @@
+def bubble_sort(data):
+    """
+    This function takes a list of numbers and return a list sorted in
+    increasing order.
+    Parameters
+    ----------
+    A list of numbers
+
+    Returns
+    -------
+    A new, sorted list, exact same numbers as the original list.
+    """
+    increasing = list(data)
+    n = len(increasing)
+    done = 1
+    for k in range(n - done):
+        for i in range(n - done):
+            compar = increasing[i + 1]
+            tested = increasing[i]
+            if tested > compar:
+                increasing[i] = compar
+                increasing[i + 1] = tested
+        done += 1
+
+    return increasing
+
+
+if __name__ == "__main__":
+
+    # I rewrite the variable name of data so as to not violate pep-8.
+    for info in ((),
+                 (1,),
+                 (1, 3, 8, 12),
+                 (12, 8, 3, 1),
+                 (8, 3, 12, 1)):
+        print('{!s:>15} --> {!s:>15}'.format(info, bubble_sort(info)))
