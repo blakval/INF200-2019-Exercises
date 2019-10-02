1fb9a6dc0e695f47413f8c0c886a84c1d7f7401e
M. K. V <mvaloy@nmbu.no>
2019-10-02 16:29:32 +0200

    Added source of median in the doc string of test_median

--- src/marie_kolvik_ex/ex03_project/test_median.py
+++ src/marie_kolvik_ex/ex03_project/test_median.py
@@ -4,10 +4,16 @@ def median(data):
 
     :param data: An iterable of containing numbers
     :return: Median of data
+
+    Source:
+    https://github.com/yngvem/INF200-2019-Exercises/blob/master/
+    exersices/ex03.rst
     """
 
     sdata = sorted(data)
     n = len(sdata)
     return (sdata[n//2] if n % 2 == 1
         else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
+
+
 
