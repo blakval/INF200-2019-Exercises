507ca2ad69ac134701a69c349a5615ac58b2229a
M. K. V <mvaloy@nmbu.no>
2019-10-02 16:51:01 +0200

    Made four tests for the median function, still no failures.

--- src/marie_kolvik_ex/ex03_project/test_median.py
+++ src/marie_kolvik_ex/ex03_project/test_median.py
@@ -1,6 +1,7 @@
 __author__ = 'Marie Kolvik Valøy'
 __email__ = 'mvaloy@nmbu.no'
 
+
 def median(data):
     """
     Returns median of data.
@@ -19,3 +20,21 @@ def median(data):
         else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
 
 
+def test_median_single():
+    """Tests that the median returns correct value for an one-element list."""
+    assert median([5]) == 5, 'Function failed test_median_single.'
+
+
+def test_median_odd():
+    """Tests that the median works for lists with odd numbers of elements"""
+    assert median([4, 6, 7]) == 6, 'Function failed test_median_odd.'
+
+
+def test_median_even():
+    """Tests that the median works for lists with even numbers of elements"""
+    assert median([4, 6, 7, 1000]) == 6.5, 'Function failed test_median_even.'
+
+
+def test_median_sorted():
+    """Tests if the median works for lists with ordered elements"""
+    assert median([1, 2, 3, 4, 5]) == 3, 'Function failed thest_media_sorted.'
