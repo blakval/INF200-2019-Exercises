0a87392d55ed508c6ab8eb8642e09542c9bd5b74
M. K. V <mvaloy@nmbu.no>
2019-10-23 11:53:53 +0200

    Made the class ListRand

--- src/marie_kolvik_ex/ex04/myrand.py
+++ src/marie_kolvik_ex/ex04/myrand.py
@@ -13,3 +13,16 @@ class LCGRand:
         m = 2 ** 31 - 1
         self.r = (a * self.r % m)
         return self.r
+
+
+class ListRand:
+    def __init__(self, list_of_seeds):
+        self.r = list_of_seeds
+        self.n = - 1
+
+    def rand(self):
+        self.n += 1
+        if self.n == len(self.r):
+            raise RuntimeError('No more numbers')
+        else:
+            return self.r[self.n]
