3bda06d49cff910bee0eb3a4957ba9b4ae2658df
M. K. V <mvaloy@nmbu.no>
2019-10-24 13:09:23 +0200

    Imported randint and made the method one step.

--- src/marie_kolvik_ex/ex04/walker.py
+++ src/marie_kolvik_ex/ex04/walker.py
@@ -3,14 +3,24 @@
 __author__ = 'Marie Kolvik Valøy'
 __email__ = 'mvaloy@nmbu.no'
 
+from random import randint
+
 
 class Walker:
     def __init__(self, start, home):
-        self.start = start
+        self.position = start
         self.home = home
+        self.number_of_steps = 0
 
     def one_step(self):
-        pass
+        step = randint(0, 1)
+        self.number_of_steps += 1
+        if step == 0:
+            step = -1
+        if self.position == self.home:
+            return'The student is at home'
+        else:
+            self.position += step
 
     def is_at_home(self):
         pass
