4393b9f344b76f35a7c862c97f8d9431be95be03
M. K. V <mvaloy@nmbu.no>
2019-10-24 19:31:04 +0200

    Did my best to "instantiate at least one generator of each class and print a few numbers from each", not sure if I understood the task correctly.

--- src/marie_kolvik_ex/ex04/myrand.py
+++ src/marie_kolvik_ex/ex04/myrand.py
@@ -26,3 +26,19 @@ class ListRand:
             raise RuntimeError('No more numbers')
         else:
             return self.r[self.n]
+
+
+if __name__ == '__main__':
+    r = LCGRand(5)
+    r1 = r.rand()
+    r12 = r.rand()
+    r = LCGRand(103)
+    r2 = r.rand()
+    print(f'When using LCGRand: Seed five gives {r1} first time, {r12} '
+          f'second time. Seed 103 gives {r2}.')
+
+    test_liste = [8, 10, 4, 8]
+    ra = ListRand(test_liste)
+    ra1 = ra.rand()
+    ra2 = ra.rand()
+    print(f'When using ListRand: The two first numbers are {ra1} and {ra2}')
