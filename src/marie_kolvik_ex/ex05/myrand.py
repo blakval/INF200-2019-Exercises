9eb3e02f625c299ab4846aeb0c0f8772ee8660c7
M. K. V <mvaloy@nmbu.no>
2019-11-02 20:00:58 +0100

    Made a try with __next__

--- src/marie_kolvik_ex/ex05/myrand.py
+++ src/marie_kolvik_ex/ex05/myrand.py
@@ -86,4 +86,10 @@ class RandIter:
         StopIteration
             If ``self.length`` random numbers are generated.
         """
-        pass
+        if self.num_generated_numbers == self.length:
+            raise StopIteration
+        elif self.num_generated_numbers is None:
+            raise RuntimeError('You must call __iter__ before __next__')
+        else:
+            used_seed = 12
+            return LCGRand(used_seed)
