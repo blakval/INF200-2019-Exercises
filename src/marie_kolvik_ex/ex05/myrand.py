d1fd30a0a42aafc75033283a8741382e84f5d68c
M. K. V <mvaloy@nmbu.no>
2019-11-02 09:42:05 +0100

    Made the proposed updates to LCGRand and copy -pasted the RandIter from the exercise text

--- src/marie_kolvik_ex/ex05/myrand.py
+++ src/marie_kolvik_ex/ex05/myrand.py
@@ -5,11 +5,80 @@ __email__ = 'mvaloy@nmbu.no'
 
 
 class LCGRand:
+    slope = 7 ** 5
+    congruence_class = 2 ** 31 - 1
+
     def __init__(self, seed):
-        self.seed = seed
+        """
+            Initialise a linear congruence random number generator
+
+            Arguments
+            ---------
+            seed : int
+                The initial seed for the generator
+        """
+        self.hidden_state = seed
 
     def rand(self):
-        a = 7 ** 5
-        m = 2 ** 31 - 1
-        self.seed = (a * self.seed % m)
-        return self.seed
+        """
+        Generate a single random number.
+
+        Returns
+        -------
+        int
+        A random integer
+        """
+        self.hidden_state = (
+                    self.slope * self.hidden_state % self.congruence_class)
+        return self.hidden_state
+
+    def random_sequence(self, length):
+        return RandIter(self, length)
+
+
+class RandIter:
+    def __init__(self, random_number_generator, length):
+        """
+        Arguments
+        ---------
+        random_number_generator :
+            A random number generator with a ``rand`` method that
+            takes no arguments and returns a random number.
+        length : int
+            The number of random numbers to generate
+        """
+        self.generator = random_number_generator
+        self.length = length
+        self.num_generated_numbers = None
+
+    def __iter__(self):
+        """
+        Initialise the iterator.
+
+        Returns
+        -------
+        self : RandIter
+
+        Raises
+        ------
+        RuntimeError
+            If iter is called twice on the same RandIter object.
+        """
+        pass
+
+    def __next__(self):
+        """
+        Generate the next random number.
+
+        Returns
+        -------
+        int
+            A random number.
+        Raises
+        ------
+        RuntimeError
+            If the ``__next__`` method is called before ``__iter__``.
+        StopIteration
+            If ``self.length`` random numbers are generated.
+        """
+        pass
